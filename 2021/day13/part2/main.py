b3fe31229415c1a3473e1ea0e3bad9512808f956
Daniel Song <dan.w.song@gmail.com>
2021-12-13 00:18:41 -0500

    Day 13

---
+++ 2021/day13/part2/main.py
@@ -0,0 +1,32 @@
+top, bottom = open('input.txt').read().split('\n\n')
+
+points = []
+for line in top.split('\n'):
+    x, y = line.split(',')
+    x, y = int(x), int(y)
+    points.append((x, y))
+
+for line in bottom.split('\n'):
+    new_points = set()
+    _, _, temp = line.split(' ')
+    axis, num = temp.split('=')
+    num = int(num)
+    for x, y in points:
+        if axis == 'x':
+            if x > num:
+                new_points.add((2 * num - x, y))
+            else:
+                new_points.add((x, y))
+        else:
+            if y > num:
+                new_points.add((x, 2 * num - y))
+            else:
+                new_points.add((x, y))
+    points = list(new_points)
+
+grid = [[' ' for c in range(40)] for r in range(10)]
+for r, c in points:
+    grid[c][r] = '*'
+
+for r in grid:
+    print(' '.join(r))
